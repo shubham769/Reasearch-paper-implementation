79ea41b41b882c08ca199e8cd05073d9e432c97d
shubham <shubhamgoyal769@gmail.com>
2020-07-29 23:44:54 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,40 @@
+# Serve model as a flask application
+
+import pickle
+import numpy as np
+from flask import Flask, request
+
+model = None
+app = Flask(__name__)
+
+
+def load_model():
+    global model
+    # model variable refers to the global variable
+    with open('model1.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+
+@app.route('/')
+def home_endpoint():
+    return 'Hello World!'
+
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Works only for a single sample
+    if request.method == 'POST':
+        data = request.get_json()  # Get data posted as a json
+        data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
+        prediction = model.predict(data.reshape(1, -1))  # runs globally loaded model on the data
+        if prediction[0] == 0:
+            output = 'no bug'
+        if prediction[0] == 1:
+            output = 'bug'
+            
+    return str(output)
+
+
+if __name__ == '__main__':
+    load_model()  # load model at the beginning once only
+    app.run(host='0.0.0.0', port=5000)
